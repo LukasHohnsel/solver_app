d611ee343e4cc329fe2efedb672f827a524e281d
Lukas Hohnsel <lukashohnsel@Lukass-MacBook-Pro.local>
2013-06-28 10:15:49 +0200

    Grundgerüst

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolverApp::Application.config.secret_token = '1081cf72a371159fbbf1729aba61a85bae419467f68e738b12f1245ca20e3bf6e9d22440f74b05e7f1cb18622a03bf409fa2e86e6025d5eb780b35e140f1282f'
